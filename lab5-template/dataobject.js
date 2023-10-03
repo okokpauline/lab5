2df085441aca48736a8d162ec00dd7bb5a2cca6a
okokpauline <125172477+okokpauline@users.noreply.github.com>
2023-10-03 13:06:54 -0700

    Add files via upload

---
+++ lab5-template/dataobject.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const courseInfo = {
+    title: "JavaScript for Web Development",
+    term: "Fall 2023",
+    sectionNumber: "CS-175-01",
+    description: "Learn how to develop modern web application using JavaScript, jQuery, AJAX, HTML, and CSS",
+    credits: 4.00,     
+    students: [
+        { firstName: "Joe", lastName: "Smith", score: 88 },
+        { firstNme: "John", lastName: "Doe", score: 65 },
+        { firstName: "Joel", lastName: "Garcia", score: 98 },
+        { firstName: "Judy", lastName: "Johnson", score: 77 },
+        { firstName: "Anne", lastName: "Dew", score: 88 }
+    ]
+};
